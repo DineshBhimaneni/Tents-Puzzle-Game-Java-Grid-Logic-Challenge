dbf51261f42cd606e7896cbb7e1e692567bd28ee
DineshBhimaneni <185594813+DineshBhimaneni@users.noreply.github.com>
2025-12-26 15:06:19 +0530

    Delete TentsGamee directory

--- TentsGamee/src/tents/CellType.java
+++
@@ -1,4 +0,0 @@
-package tents;
-public enum CellType {
-    EMPTY, TREE, TENT
-}
--- TentsGamee/src/tents/Main.java
+++
@@ -1,8 +0,0 @@
-package tents;
-import javax.swing.SwingUtilities;
-
-public class Main {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new TentsUI().setVisible(true));
-    }
-}
